ea61d877a0842e5feb6c6b16e78d9073ee0cf1f7
chealse-ma <chealsemafaune@gmail.com>
2024-01-19 20:20:11 +0000

    changed everywhere

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-function updateWeather(response) {
-  let temperatureElement = document.querySelector("#weather-temperature");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
+  let cityElement = document.querySelector("#weather-app-city");
   let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
+  let humdityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  let iconElement = document.querySelector("#weather-icon");
 
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon"/>`;
-  console.log(response);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-temperature-icon"
+              />`;
+
   cityElement.innerHTML = response.data.city;
+
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humdityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
+
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -23,16 +29,16 @@ function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
   ];
-  let day = days[date.getDay()];
 
+  let day = days[date.getDay()];
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -41,18 +47,45 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "d5cf5f78a130388886eo4652tf8e3a0b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(updateWeather);
+  let apiKey = "0c9a9eb6fa633t1c3o446ff55ab61320";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `          <div class="row">
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                alt=""
+                width="40"
+              />
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature-max">18°</div>
+                <div class="weather-forecast-temperature-min">12°</div>
+              </div>
+            </div>
+          </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("London");
+searchCity("Shipley");
+displayForecast();
