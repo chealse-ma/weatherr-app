1555397375bb1a19f2b4237b54a78a9a16495ffe
chealse-ma <chealsemafaune@gmail.com>
2024-01-19 20:14:10 +0000

    css and javascript

--- src/index.js
+++ src/index.js
@@ -1,69 +1,58 @@
-function displaySearch(response) {
-  let displayTemp = document.querySelector("#tempValue");
-  let temp = Math.round(response.data.temperature.current);
-  displayTemp.innerHTML = `${temp}°C`;
-
-  let iconElement = document.querySelector("#icon");
-  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.condition.description;
-
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `${response.data.temperature.humidity}% `;
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#weather-icon");
 
-  let windSpeedElement = document.querySelector("#windSpeed");
-  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
-
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon"/>`;
   console.log(response);
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
-let searchForm = document.querySelector("#city-form");
-searchForm.addEventListener("submit", search);
-
-function search(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#enterCity");
-  let city = cityInputElement.value;
-  fetchCurrentWeather(city);
-}
-
-function fetchCurrentWeather(city) {
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = city;
-
-  let apiKey = "53980o1ef09d7553cet92b43aefbc155";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiURL).then(displaySearch);
-}
-
-fetchCurrentWeather("Sintra");
 
 function formatDate(date) {
-  let hour = currentTime.getHours();
-  let minutes = currentTime.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hour < 10) {
-    hour = `0${hour}`;
-  }
-  let day = currentTime.getDay();
-
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
   let days = [
-    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
+    "Sunday",
   ];
-  let formattedDay = days[day];
+  let day = days[date.getDay()];
 
-  return `${formattedDay} ${hour}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-let currentDate = document.querySelector("#current-date");
-let currentTime = new Date();
-currentDate.innerHTML = formatDate(currentTime);
+function searchCity(city) {
+  let apiKey = "d5cf5f78a130388886eo4652tf8e3a0b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
