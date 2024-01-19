668931296956b89ea57137fe70be886b1cd51d4d
chealse-ma <chealsemafaune@gmail.com>
2024-01-19 18:50:58 +0000

    changed javascipt

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
-  temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-  let apiKey = "e46a1c61e5cf3a8dt9e82o738b302524";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key= ${apiKey}";
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -23,3 +22,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
